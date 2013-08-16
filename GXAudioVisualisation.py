4789374d52b584ed58046d798e2bdee53567a023
Sławomir Kur <gethiox@gmail.com>
2013-08-16 23:32:30 +0200

    0.31 version

--- GXAudioVisualisation.py
+++ GXAudioVisualisation.py
@@ -2,18 +2,6 @@
 # version 0.31 
 # 16.08.2013
 
-"""
-# CHANGELOG:
-# update comments and informations
-# a little clearifications in code
-# add choosing channels to generate visualisation (stereo variable)
-# no one bug has been fixed, because all of my code is one big crap
-# TODO:
-# repair low frequnecy range variable (low)
-# add scaling bar mode and transform bar mode of visualisation (currlency only array mode)
-# gui!
-# I don't know what next
-"""
 import bpy
 
 #############
